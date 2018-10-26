bcdc3576e12581ac1e1350992eaeee45f3a5a345
Merewanj <mehe_jesa@yahoo.com>
2018-10-25 21:13:14 -0700

    Login bottons added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
+import Header from './Header';
 
 class App extends Component {
+
+  constructor (props){
+    super(props);
+    this.state = {
+      color: 'RED',
+      banner: 'Login',
+      banner2: 'Password',
+      isOpen: false,
+
+    };
+    this.buttonHandler = this.buttonHandler.bind(this);
+
+    this.textHandler = this.textHandler.bind(this);
+  }
+
+  textHandler(e){
+    this.setState({
+      banner: e.target.value,
+    });
+  }
+
+  buttonHandler(){
+    //console.log('my botton is working')
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+
+  }
   render() {
+    let myVariable = <h2>Merewan</h2>;
+    let myBanner;
+    if(this.state.isOpen){
+      myBanner = <Header banner={this.state.banner}/>;
+
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {myVariable}
           </p>
-          <h2>413 Starter Kit </h2>
+          {myBanner}
+          {
+            <Header banner={this.state.banner}/>
+          }
+          <input value={this.state.banner} onChange={this.textHandler}></input>
+          <input value={this.state.banner2} onChange={this.textHandler}></input>
+          
+          <button onClick={this.buttonHnadler}>Login</button>
         </header>
       </div>
     );
