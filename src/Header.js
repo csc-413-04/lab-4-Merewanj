5e50716e12de3324504d9ec498468484849886e7
Merewanj <mehe_jesa@yahoo.com>
2018-10-25 21:14:07 -0700

    Header Created

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render () {
+        return (
+        <div>
+            <h1>Copper Bot</h1>
+            {this.props.banner}
+        </div>
+
+        );
+    }
+}
+
+
+export default Header;
